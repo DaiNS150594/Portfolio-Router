7e48dd4c1f029ed5a7bce2321821bab8ad4f8d61
DaiNS <dai.nguyensi@euroland.com>
2022-08-18 17:34:39 +0700

    Update to FAQ html

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import Banner from '../../components/Banner'
+import Programs from '../../components/Programs'
+import Values from '../../components/Values'
+import FAQs from '../../components/FAQs'
 import './home.css'
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <Programs />
+      <Values />
+      <FAQs />
     </>
   )
 }
