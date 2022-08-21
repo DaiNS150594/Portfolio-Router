6d8af2fdbd91baadb1b90973995c754a4ad943a9
DaiNS150594 <dains150594@gmail.com>
2022-08-21 23:50:13 +0700

    Update testimonials

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,17 @@ import Banner from '../../components/Banner'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
 import FAQs from '../../components/FAQs'
+import Testimonials from '../../components/Testimonials'
 import './home.css'
 
 const Home = () => {
   return (
     <>
-      <Banner />
-      <Programs />
-      <Values />
-      <FAQs />
+      <Banner/>
+      <Programs/>
+      <Values/>
+      <FAQs/>
+      <Testimonials/>
     </>
   )
 }
